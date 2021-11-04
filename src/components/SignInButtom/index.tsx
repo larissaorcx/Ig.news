ed59c1473a484f2d960cd50b5f64a0894fcdcbbf
Ingrid Larissa <larissaorcx@gmail.com>
2021-11-04 16:12:41 -0300

    Configurando o banco de dados FaunaDB

--- src/components/SignInButtom/index.tsx
+++ src/components/SignInButtom/index.tsx
@@ -1,21 +1,24 @@
 import style from './styles.module.scss'
 
+import { signIn, signOut, useSession } from 'next-auth/client'
+
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
 
 export function SingInButtom(){
-    const isUserLoggerIn = true;
+    const [session] = useSession();
 
     return(
-        isUserLoggerIn ?(
-            <button type="button" className={style.SingInButtom}>
+        session ?(
+            <button type="button" className={style.SingInButtom} onClick={() => signOut()}>
             <FaGithub color="#04d361"/>
-            Ingrid Oliveira
+            {session.user.name}
 
             <FiX color="#737380" className={style.closeIncon} />
             </button>
         ):(
-            <button type="button" className={style.SingInButtom}>
+            <button type="button" className={style.SingInButtom} onClick={() => signIn('github')}>
+
             <FaGithub color="#eba417"/>
             Sign In With Github
             </button>
