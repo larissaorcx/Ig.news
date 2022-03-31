263a2cbe00a04edcda1e6f18a55e078596b19736
Ingrid Larissa <larissaorcx@gmail.com>
2022-03-31 00:25:09 -0300

    coverage report

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,21 @@
 module.exports = {
     testPathIgnorePatterns: ["/node_modules/", "/.next/"],
     setupFilesAfterEnv: [
-        "<rootDir>/src/tests/setupTests.ts"
+      "<rootDir>/src/tests/setupTests.ts"
     ],
     transform: {
-        "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest"
+      "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest"
     },
+    testEnvironment: 'jsdom',
     moduleNameMapper: {
-        "\\.(scss|css|sass)$": "identity-obj-proxy"
+      "\\.(scss|css|sass)$": "identity-obj-proxy"
     },
-    testEnvironment: 'jsdom', //ambiente para a aplicação
-}
+    collectCoverage: true,
+    collectCoverageFrom: [
+      "src/**/*.tsx",
+      "!src/**/*.spec.tsx", //exclui cobertura
+      "!src/**/_app.tsx", //exclui cobertura
+      "!src/**/_document.tsx", //exclui cobertura
+    ],
+    coverageReporters: ["lcov", "json"] //feedback de cobertura
+  };
